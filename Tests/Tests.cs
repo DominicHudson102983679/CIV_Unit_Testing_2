75d2c5ae0f41b362512567faf74d1557807f29c3
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-08-23 17:04:25 +1000

    added references and Using MiscLib

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,10 +1,18 @@
 using System;
 using Xunit;
+using MiscLib;
 
 namespace Tests
 {
     public class MiscLibTests
     {
-        // Write Unit Tests for all functions for your assigned group
+        [Theory]
+        [InlineData(5, 10, 50)]
+        [InlineData(4, 7, 60)] 
+        [InlineData(3, 5, 70)] 
+        public void CalcRectAreaTest(int length, int width, int result)
+        {
+            var test = new GroupOneFunctions();
+        }
     }
 }
